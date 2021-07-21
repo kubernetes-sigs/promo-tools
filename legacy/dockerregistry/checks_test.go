fa6e2d16b5dea7ba8e40792bec0bf8a9ab925121
Linus Arver <linusa@google.com>
2021-07-21 07:34:56 -0700

    checks_test.go: revert https://github.com/kubernetes/release/pull/1772

--- legacy/dockerregistry/checks_test.go
+++ legacy/dockerregistry/checks_test.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	grafeaspb "google.golang.org/genproto/googleapis/grafeas/v1"
 
 	reg "sigs.k8s.io/k8s-container-image-promoter/legacy/dockerregistry"
@@ -181,14 +180,16 @@ func TestImageRemovalCheck(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		masterEdges, err := reg.ToPromotionEdges(test.masterManifests)
-		require.Nil(t, err)
-
-		pullEdges, err := reg.ToPromotionEdges(test.pullManifests)
-		require.Nil(t, err)
-
+		// TODO: Why are we not checking errors here?
+		// nolint: errcheck
+		masterEdges, _ := reg.ToPromotionEdges(test.masterManifests)
+		// TODO: Why are we not checking errors here?
+		// nolint: errcheck
+		pullEdges, _ := reg.ToPromotionEdges(test.pullManifests)
 		got := test.check.Compare(masterEdges, pullEdges)
-		require.Equal(t, got, test.expected)
+		err := checkEqual(got, test.expected)
+		checkError(t, err,
+			fmt.Sprintf("checkError: test: %v imageRemovalCheck\n", test.name))
 	}
 }
 
@@ -336,10 +337,19 @@ func TestImageSizeCheck(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		var err error
-		test.check.PullEdges, err = reg.ToPromotionEdges(test.manifests)
-		require.Nil(t, err)
-		require.Len(t, test.check.PullEdges, len(test.imageSizes))
+		// TODO: Why are we not checking errors here?
+		// nolint: errcheck
+		test.check.PullEdges, _ = reg.ToPromotionEdges(test.manifests)
+		err := checkEqual(len(test.check.PullEdges), len(test.imageSizes))
+
+		checkError(
+			t,
+			err,
+			fmt.Sprintf(
+				"checkError: test: %v Number of image sizes should be equal to the number of edges (ImageSizeCheck)\n",
+				test.name,
+			),
+		)
 
 		for edge := range test.check.PullEdges {
 			test.check.DigestImageSize[edge.Digest] =
@@ -347,7 +357,13 @@ func TestImageSizeCheck(t *testing.T) {
 		}
 
 		got := test.check.Run()
-		require.Equal(t, got, test.expected)
+		err = checkEqual(got, test.expected)
+
+		checkError(
+			t,
+			err,
+			fmt.Sprintf("checkError: test: %v (ImageSizeCheck)\n", test.name),
+		)
 	}
 }
 
@@ -567,8 +583,9 @@ func TestImageVulnCheck(t *testing.T) {
 			test.severityThreshold,
 			mkVulnProducerFake(test.vulnerabilities),
 		)
-
 		got := check.Run()
-		require.Equal(t, got, test.expected)
+		err := checkEqual(got, test.expected)
+		checkError(t, err,
+			fmt.Sprintf("checkError: test: %v (ImageVulnCheck)\n", test.name))
 	}
 }
