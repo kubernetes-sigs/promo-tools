2c61c8c5b044d0ccc7e697fc747e13f20f2945e0
tylerferrara <tylerferrara123@gmail.com>
2021-07-22 13:49:08 -0400

    Log request counter every 10min

--- legacy/reqcounter/reqcounter.go
+++ legacy/reqcounter/reqcounter.go
@@ -16,13 +16,43 @@ limitations under the License.
 
 package reqcounter
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
 
 // RequestCounter records the number of HTTP requests to GCR.
 type RequestCounter struct {
-	mutex    sync.Mutex
-	requests uint64
+	mutex    sync.Mutex // Lock to prevent race-conditions with concurrent processes.
+	requests uint64     // Number of HTTP requests since recording started.
+	since    time.Time  // When the current counter began recording requests.
 }
+
+// log records the number of HTTP requests found and resets the counter.
+func (rc *RequestCounter) log() {
+	// Hold onto the lock when reading & writing the counter.
+	rc.mutex.Lock()
+	defer rc.mutex.Unlock()
+
+	// Log the number of requests within this measurement window.
+	msg := fmt.Sprintf("Since %s there have been %d requests to GCR.", counter.since.Format("2006-01-02 15:04:05"), rc.requests)
+	logrus.Debug(msg)
+
+	// Reset the counter.
+	rc.requests = 0
+	rc.since = time.Now()
+}
+
+const (
+	// measurementWindow specifies the length of time to wait before logging the RequestCounter. Since Google's
+	// Container Registry specifies a quota of 50,000 HTTP requests per 10 min, the window
+	// for recording requests is set to 10 min.
+	// Source: https://cloud.google.com/container-registry/quotas
+	measurementWindow = time.Minute * 10
+)
 
 var (
 	// enableCounting will only become true if the Init function is called. This allows
@@ -30,16 +60,19 @@ var (
 	enableCounting = false
 	// counter will continuously be modified by the Increment function to count all
 	// HTTP requests to GCR.
-	counter = RequestCounter{}
+	counter = &RequestCounter{}
 )
 
 // Init allows request counting to begin.
 func Init() {
 	enableCounting = true
-	counter = RequestCounter{
+	counter = &RequestCounter{
 		mutex:    sync.Mutex{},
 		requests: 0,
+		since:    time.Now(),
 	}
+	// Trigger the logger to run in the background.
+	go requestLogger(measurementWindow)
 }
 
 // Increment increases the request counter by 1, signifying an HTTP
@@ -49,5 +82,13 @@ func Increment() {
 		counter.mutex.Lock()
 		counter.requests++
 		counter.mutex.Unlock()
+	}
+}
+
+// requestLogger continuously logs the number of recorded HTTP requests every interval.
+func requestLogger(interval time.Duration) {
+	for {
+		time.Sleep(interval)
+		counter.log()
 	}
 }
