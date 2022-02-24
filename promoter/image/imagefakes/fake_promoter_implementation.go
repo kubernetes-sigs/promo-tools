4c2c3105960ae6614676663645df17640df8d656
Stephen Augustus <foo@auggie.dev>
2022-02-24 04:08:37 -0500

    generated: Run `make update-mocks`
    Signed-off-by: Stephen Augustus <foo@auggie.dev>

--- promoter/image/imagefakes/fake_promoter_implementation.go
+++ promoter/image/imagefakes/fake_promoter_implementation.go
@@ -20,8 +20,8 @@ package imagefakes
 import (
 	"sync"
 
-	imagepromoterb "sigs.k8s.io/promo-tools/v3/internal/promoter/image"
 	inventory "sigs.k8s.io/promo-tools/v3/internal/legacy/dockerregistry"
+	imagepromoterb "sigs.k8s.io/promo-tools/v3/internal/promoter/image"
 	imagepromotera "sigs.k8s.io/promo-tools/v3/promoter/image/options"
 )
 
