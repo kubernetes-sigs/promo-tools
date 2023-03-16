5170cc001d527bf7bc5795ff67955f14845aefc5
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2023-03-15 19:11:25 -0600

    Add ratelimiter package
    This commit adds a ratelimiter packaged based on
    geranos' roundtripper:
    
    https://github.com/kubernetes/registry.k8s.io/blob/main/cmd/geranos/ratelimitroundtrip.go
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.8.2
 	golang.org/x/oauth2 v0.6.0
+	golang.org/x/time v0.2.0
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
 	google.golang.org/api v0.112.0
 	google.golang.org/genproto v0.0.0-20230303212802-e74f57abe488
@@ -281,7 +282,6 @@ require (
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
-	golang.org/x/time v0.2.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
---
+++ promoter/image/ratelimit/roundtripper.go
@@ -0,0 +1,53 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ratelimit
+
+import (
+	"context"
+	"net/http"
+
+	"golang.org/x/time/rate"
+)
+
+const burst = 1
+
+// RoundTripper wraps an http.RoundTripper with rate limiting
+type RoundTripper struct {
+	rateLimiter  *rate.Limiter
+	roundTripper http.RoundTripper
+}
+
+var _ http.RoundTripper = &RoundTripper{}
+
+func NewRoundTripper(limit rate.Limit) *RoundTripper {
+	return &RoundTripper{
+		rateLimiter:  rate.NewLimiter(limit, burst),
+		roundTripper: http.DefaultTransport,
+	}
+}
+
+func (rt *RoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	// only rate limit read type calls, not writes
+	if r.Method == http.MethodGet || r.Method == http.MethodHead {
+		err := rt.rateLimiter.Wait(context.Background())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return rt.roundTripper.RoundTrip(r)
+}
