d036e497714ed04ffd341447f41d43cb1a6cc398
cpanato <ctadeu@gmail.com>
2023-06-07 10:40:40 +0200

    update log for verifying the certificates flags
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- internal/legacy/cli/run.go
+++ internal/legacy/cli/run.go
@@ -69,7 +69,9 @@ func RunPromoteCmd(opts *options.Options) error {
 	})
 	logrus.AddHook(NewHook())
 
-	logrus.Infof("Options for certificates: certificate id: %s | cert id regexp: %s | cert issuer: %s | cert issuer regexp: %s", opts.SignCheckIdentity, opts.SignCheckIdentityRegexp, opts.SignCheckIssuer, opts.SignCheckIssuerRegexp)
+	logrus.Infof("Options to check the Signatures: SignCheckIdentity: %s | SignCheckIdentityRegexp: %s | SignCheckIssuer: %s | SignCheckIssuerRegexp: %s",
+		opts.SignCheckIdentity, opts.SignCheckIdentityRegexp, opts.SignCheckIssuer, opts.SignCheckIssuerRegexp,
+	)
 
 	// Mode 1: Manifest list verification (removed)
 
