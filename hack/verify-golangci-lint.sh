90a8d5f4ce0a47cba2113e917ae5bb4fbcbde26a
Carlos Panato <ctadeu@gmail.com>
2025-05-12 14:36:49 +0200

    migrate golangci-lint to v2 and update runner script
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- hack/verify-golangci-lint.sh
+++ hack/verify-golangci-lint.sh
@@ -18,9 +18,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-VERSION=v1.61.0
+VERSION=v2.1.6
 URL_BASE=https://raw.githubusercontent.com/golangci/golangci-lint
-URL=$URL_BASE/$VERSION/install.sh
+URL=${URL_BASE}/${VERSION}/install.sh
+# If you update the version above you might need to update the checksum
+# if it does not match. We say might because in the past the install script
+# has been unchanged even if there is a new verion of golangci-lint.
+# To obtain the checksum, download the install script and run the following
+# command:
+# > sha256sum <path-to-install-script>
+INSTALL_CHECKSUM=9e99d38f3213411a1b6175e5b535c72e37c7ed42ccf251d331385a3f97b695e7
 
 if [[ ! -f .golangci.yml ]]; then
     echo 'ERROR: missing .golangci.yml in repo root' >&2
@@ -28,8 +35,17 @@ if [[ ! -f .golangci.yml ]]; then
 fi
 
 if ! command -v golangci-lint; then
-    curl -sfL $URL | sh -s $VERSION
-    PATH=$PATH:bin
+    INSTALL_SCRIPT=$(mktemp -d)/install.sh
+    curl -sfL "${URL}" >"${INSTALL_SCRIPT}"
+    if echo "${INSTALL_CHECKSUM} ${INSTALL_SCRIPT}" | sha256sum --check; then
+        chmod 755 "${INSTALL_SCRIPT}"
+        ${INSTALL_SCRIPT} -b /tmp "${VERSION}"
+        export PATH=${PATH}:/tmp
+        pwd
+    else
+        echo 'ERROR: install script sha256 checksum invalid' >&2
+        exit 1
+    fi
 fi
 
 golangci-lint version
